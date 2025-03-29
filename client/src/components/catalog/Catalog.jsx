cf998681480abe2091d238846221fdedba013043
shefket-mustafa <miracle_1@abv.bg>
2025-03-29 18:15:44 +0200

    Search option and functionality added to the catalog

--- client/src/components/catalog/Catalog.jsx
+++ client/src/components/catalog/Catalog.jsx
@@ -1,9 +1,28 @@
 import '../catalog/catalog.css';
-import { useBooks } from '../../utils-book-API/bookApi';
+import { useBooks, useSearchBooks } from '../../utils-book-API/bookApi';
 import CatalogItems from './catalog-item/CatalogItems';
+import { useEffect, useState } from 'react';
 
 export default function Catalog() {
+
+    const [searchValue, setSearchValue] = useState('');
+    const [filteredBooks, setFilteredBooks] = useState([]);
     const  {books}  = useBooks();
+    const {search} = useSearchBooks();
+
+    useEffect(() => {
+      setFilteredBooks(books);
+    }, [books]);
+
+    const onSearch = async() => {
+      if(searchValue.trim() === ''){
+        setFilteredBooks(books)
+        return
+      } 
+        const result = await search(searchValue)
+        setFilteredBooks(result)
+    } 
+
     
     
   return (
@@ -11,11 +30,27 @@ export default function Catalog() {
       <div className="booklist-bg-box"></div>
       <div className="catalog-content">
         <div className="catalog-heading">
-          <h2>Available Books</h2>
+        <h2>Search Books</h2>
+          <input
+            type="text"
+            placeholder="Search by title..."
+            // value={searchTerm}
+            onChange={(e) => {
+              setSearchValue(e.target.value)
+            }
+            } 
+            className="search-input"
+          />
+          <button onClick={onSearch} className='search-btn'>Search</button>
         </div>
         <div className="book-grid">
-          {books.map((book) => <CatalogItems key={book._id} book={book}/>)}
+          {filteredBooks.length > 0 ? filteredBooks.map((book) => <CatalogItems key={book._id} book={book}/>) : 
+          <div>
+          <h1>Could not find your book!</h1>
         </div>
+          }
+        </div>
+        
       </div>
     </section>
   );
