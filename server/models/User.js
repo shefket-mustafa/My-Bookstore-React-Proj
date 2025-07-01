94f93506481a142ac7abd3c61d77bbd65df511d8
shefket-mustafa <miracle_1@abv.bg>
2025-07-01 21:23:35 +0300

    task/created user modal to use in the backend authentication routes

---
+++ server/models/User.js
@@ -0,0 +1,9 @@
+
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    email: {type: String, required: true, unique: true},
+    hashedPassword: {type: String, required: true},
+})  
+
+export default mongoose.model('User', userSchema);
